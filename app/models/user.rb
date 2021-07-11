568ec7a2c7cce0a3c2eeea1f8eec2c3ae47a8c62
KayKawa <goodenoughliving1221@gmail.com>
2021-07-11 21:19:23 +0900

    Userモデルのバリデーション記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise  :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  with_options presence: true do
+    # 本名のバリデーション
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+    # 本名(カナ)のバリデーション
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
 
-  validates :nickname, presence: true
-  validates :password,  { presence: true, length: { minimum: 6 },
-                          format: { with: VALID_PASSWORD_REGEX } }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :birthday, presence: true
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
+    validates :birthday
+  end
 end
