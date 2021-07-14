247d2506e025b8b68236d84fd8e7049a50419442
KayKawa <goodenoughliving1221@gmail.com>
2021-07-14 10:08:08 +0900

    Active Storageアソシエーションとitemsストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:name, :content, :category_id, :condition_id, :cost_id, :shipping_area_id, :shipping_day_id, :price,
+                                  :image).merge(user_id: current_user.id)
+  end
 end
