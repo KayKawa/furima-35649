45f55e8dd2aa823e1f607182f5a1281abe6f145e
KayKawa <goodenoughliving1221@gmail.com>
2021-07-14 21:07:30 +0900

    itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,9 @@ class Item < ApplicationRecord
 
   # 空の投稿を保存できないようにする
   with_options presence: true do
-    validates :name
-    validates :content
-    validates :price,
-              numericality: { less_than_or_equal_to: 9_999_999, greater_than_or_equal_to: 300,
-                              message: 'Please specify between 300 and 9,999,999.' }
     validates :image
+    validates :name, length: { maximum: 40 } # 必須40文字まで
+    validates :content, length: { maximum: 1000 } # 必須1000文字まで
     # ActiveHashの選択が「--」の時は保存できないようにする
     with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
@@ -20,5 +17,10 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :shipping_day_id
     end
+    validates :price
   end
+  # 価格の範囲指定
+  validates :price, inclusion: { in: 300..9_999_999, message: 'please specify between 300 and 9,999,999.' }
+  # 価格は半角数字のみ
+  validates :price_before_type_cast, format: { with: /\A[0-9]+\z/, message: 'input half-width characters.' }
 end
