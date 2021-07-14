3cef5e028ac60ee54c0ca4e6dba6f8e601c996c4
KayKawa <goodenoughliving1221@gmail.com>
2021-07-14 14:09:21 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,20 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :cost, :prefecture, :shipping_day
+
+  # 空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :price, numericality: { in: 300..9_999_999, message: 'Please specify between 300 and 9,999,999.' }
+    validates :image
+    # ActiveHashの選択が「--」の時は保存できないようにする
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, { message: "Category can't be blank" }
+      validates :condition_id, { message: "Condition can't be blank" }
+      validates :cost_id, { message: "Cost can't be blank" }
+      validates :prefecture_id, { message: "Predecture can't be blank" }
+      validates :shipping_day_id, { message: "Shipping_day can't be blank" }
+    end
+  end
 end
