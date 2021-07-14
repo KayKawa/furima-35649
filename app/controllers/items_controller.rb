638f2b4c014bc2ef0144cf1a8003f579c2468af9
KayKawa <goodenoughliving1221@gmail.com>
2021-07-14 15:41:35 +0900

    出品機能のcreateアクション作成とviewファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    item = Item.create(items_params)
+    if item
+      render :index
+    else
+      redirect_to :new_item_path
+    end
+  end
+
   private
 
   def items_params
-    params.require(:items).permit(:name, :content, :category_id, :condition_id, :cost_id, :shipping_area_id, :shipping_day_id, :price,
-                                  :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :content, :category_id, :condition_id, :cost_id, :prefecture_id, :shipping_day_id, :price,
+                                 :image).merge(user_id: current_user.id)
   end
 end
