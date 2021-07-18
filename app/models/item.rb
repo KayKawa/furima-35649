3576149026444c7da0093758e07b8441e78da9b7
KayKawa <goodenoughliving1221@gmail.com>
2021-07-18 11:41:19 +0900

    出品商品の表示とダミー表示の条件分岐

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,11 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :cost, :prefecture, :shippingday
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :shippingday
 
   # 空の投稿を保存できないようにする
   with_options presence: true do
