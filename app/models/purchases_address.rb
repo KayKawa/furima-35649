d2533f8d0298ac983ef240aa4c66dc4d2ec09f38
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 10:17:57 +0900

    Formオブジェクトのクラス名変更

--- app/models/purchases_address.rb
+++ app/models/purchases_address.rb
@@ -1,4 +1,4 @@
-class PurchasesAddress
+class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :purchase, :postcode, :prefecture_id, :city, :address, :building_name, :phone_number,
                 :purchase_id
