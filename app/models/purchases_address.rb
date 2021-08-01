228b24492cdec43458cdbdba8017037651f1c33e
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 09:28:16 +0900

    バリデーションの修正

--- app/models/purchases_address.rb
+++ app/models/purchases_address.rb
@@ -1,9 +1,11 @@
 class PurchasesAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :purchase, :postcode, :prefecture_id, :city, :address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :purchase, :postcode, :prefecture_id, :city, :address, :building_name, :phone_number
 
   # 空の投稿を保存できないようにする
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' } # ３ケタの整数 + ハイフン + ４ケタの整数
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
