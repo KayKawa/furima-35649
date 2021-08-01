f2c8f3e8c21c26bc2c31df8a053d5151c3edd5d0
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 18:35:22 +0900

    pay.jp secret

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,7 +10,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
-      Payjp.api_key = 'sk_test_b1862899d86c77ed596b2059'  # 秘密鍵
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # 秘密鍵
       Payjp::Charge.create(
         amount: @item.price, # 商品の値段
         card: purchase_params[:token], # カードトークン
