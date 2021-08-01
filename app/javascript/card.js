30a99f145fc07b3226a354da0acb6901ded15e19
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 18:39:36 +0900

    pay.jp public

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_beb9fddf2270c6f7bdd8fcf5"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // PAY.JPテスト公開鍵（webpackを用いて環境変数へ）
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
