dfc9eb1eb6f51c7d76cd61f78a0e747b5062fc35
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 12:08:48 +0900

    ルーティング修正
    ・showアクションのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items do
+  resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :items, only: [:show] do
     resources :purchases, only: [:index, :create]
   end
 end
