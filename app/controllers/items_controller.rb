d424b55b360f4eece313dffa05036f7c45aa12ec
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 22:41:52 +0900

    トップページと詳細ページの修正
    ・売れている商品にはsoldoutを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_item, only: [:show, :edit, :update, :destroy]
+  before_action :find_purchase, only: [:show]
   before_action :move_to_root, only: [:edit, :update]
 
   def index
@@ -22,7 +23,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @purchase = Purchase.find(params[:id]) # 購入記録のデータを取得
   end
 
   def edit
@@ -54,6 +54,10 @@ class ItemsController < ApplicationController
 
   def find_item
     @item = Item.find(params[:id])
+  end
+
+  def find_purchase
+    @purchase = Purchase.find(params[:id]) # 購入記録のデータを取得
   end
 
   def move_to_root
