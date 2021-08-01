ccb1d38159830ec2b298b19a6ae7ab85ef75aa6e
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 09:26:08 +0900

    バリデーション修正

--- app/models/purchases_address.rb
+++ app/models/purchases_address.rb
@@ -4,7 +4,7 @@ class PurchasesAddress
 
   # 空の投稿を保存できないようにする
   with_options presence: true do
-    validates :postcode, format: { with: /\A\d{3}-d{4}\z/ } # ３ケタの整数 + ハイフン + ４ケタの整数
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' } # ３ケタの整数 + ハイフン + ４ケタの整数
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
