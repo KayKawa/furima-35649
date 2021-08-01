176ebc27724ef3e89c8f965b4da17d5dd47d95df
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 11:30:14 +0900

    Formオブジェクトを用いたアクション内容を記述
    ・ストロングパラメータ修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,18 +2,23 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def purchase_params
-    params.permit(item_id: @item.id).merge(user_id: current_user.id)
-  end
-
-  def address_params
-    params.permit(:postcode, :prefecture_id, :city, :address, :building_name, :phone_number).merge(purchase_id: @purchase.id)
+    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :address, :building_name,
+                                             :phone_number, item_id: @item_id).merge(user_id: current_user.id)
   end
 end
