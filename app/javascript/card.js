8fb67beb17ec38e7bf940d41ff1e89d2b94c97f3
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 15:12:48 +0900

    クレジット情報をトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_beb9fddf2270c6f7bdd8fcf5"); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火");
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_address[number]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+      cvc: formData.get("purchase_address[cvc]"),
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token);
+      }
+    });
   });
 };
 
