2ae6d98a0e3e54a37cc171247c29b0fcd500a57a
KayKawa <goodenoughliving1221@gmail.com>
2021-08-01 09:34:49 +0900

    複数モデルのデータをテーブルへ保存

--- app/models/purchases_address.rb
+++ app/models/purchases_address.rb
@@ -12,4 +12,13 @@ class PurchasesAddress
     validates :address
     validates :phone_number, format: { with: /\A\d{11}\z/ } # 11ケタ
   end
+
+  def save
+    # 購入情報を保存し、変数purchaseに代入する
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    # 住所を保存する
+    # donation_idには、変数donationのidと指定する
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address,
+                   building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
 end
