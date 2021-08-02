ae4638c5449049203626f3997debee346438c3f0
KayKawa <goodenoughliving1221@gmail.com>
2021-08-02 22:11:07 +0900

    FactoryBot修正
    building_name

--- spec/factories/purchase_address.rb
+++ spec/factories/purchase_address.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     prefecture_id { rand(2..48) }
     city { Gimei.city.kanji }
     address { Gimei.town.kanji }
-    building_name { '' }
+    building_name { Gimei.town.kanji }
     phone_number { Faker::Number.number(digits: 11) }
     token { 'tok_abcdefghijk22222222222222222' }
   end
