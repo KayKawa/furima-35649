14206eed6319d5a474112009fc6100e9e075a604
KayKawa <goodenoughliving1221@gmail.com>
2021-08-02 20:32:41 +0900

    コントローラの不要な記述を削除
    onlyオプションの削除

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,8 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_item, only: [:index, :create]
-  before_action :move_to_root, only: [:index, :create]
-  before_action :sold_out_item, only: [:index, :create]
+  before_action :find_item
+  before_action :move_to_root
+  before_action :sold_out_item
 
   def index
     @purchase_address = PurchaseAddress.new
