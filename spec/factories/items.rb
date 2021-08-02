4db5ee64096439d5f31ac62add3dfec8907dffff
KayKawa <goodenoughliving1221@gmail.com>
2021-08-02 21:26:23 +0900

    単体テストコードの修正
    ・不要なidの定義の削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    id { 1 }
     name { Faker::Commerce.product_name }
     content { '商品説明文です。' }
     category_id { rand(2..11) }
@@ -9,10 +8,7 @@ FactoryBot.define do
     prefecture_id { rand(2..48) }
     schedule_id { rand(2..4) }
     price { rand(300..9_999_999) }
-    user_id { @user.id }
-
     association :user
-
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/sample.jpg'), filename: 'sample.jpg')
     end
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
   before do
-    @user = FactoryBot.build(:user)
-    @item = FactoryBot.build(:item, user_id: @user.id)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
     @purchase_address = FactoryBot.build(:purchase_address, user_id: @user.id, item_id: @item.id)
+    sleep 0.1
   end
 
   describe '商品購入' do
