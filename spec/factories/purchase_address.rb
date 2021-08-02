ad235599bab665cf134b6b1d119c49cd1ed86f21
KayKawa <goodenoughliving1221@gmail.com>
2021-08-02 20:47:55 +0900

    FactoryBotの不要記述の削除
    ・モデル単体テストコード内に同義記述ありのため

--- spec/factories/purchase_address.rb
+++ spec/factories/purchase_address.rb
@@ -7,9 +7,5 @@ FactoryBot.define do
     building_name { '' }
     phone_number { Faker::Number.number(digits: 11) }
     token { 'tok_abcdefghijk22222222222222222' }
-    user_id { @user.id }
-    item_id { @item.id }
-    association :user  # users.rbのFactoryBotとアソシエーション
-    association :item  # items.rbのFactoryBotとアソシエーション
   end
 end
