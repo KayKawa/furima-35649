23a676c62bc98ee873af4a0b2a9d1cf4c7d185d8
KayKawa <goodenoughliving1221@gmail.com>
2021-08-03 14:42:10 +0900

    ルーティングのリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :items, only: [:show] do
+  resources :items do
     resources :purchases, only: [:index, :create]
   end
 end
